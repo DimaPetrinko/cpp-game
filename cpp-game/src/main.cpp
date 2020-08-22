3d4b16a85554e6fa261fe14a8a87a309702ba378
Dmytrii Petrinko <petrinko6@gmail.com>
2020-08-22 19:44:32 +0300

    Window

--- cpp-game/src/main.cpp
+++ cpp-game/src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "OGL.h"
 
 int main()
 {
 	std::cout << "Hello, cpp-game\n";
+	OGL ogl = OGL(640, 320);
+
+	while (!ogl.IsWindowClosed())
+	// while (ogl.IsWindowClosed())
+	{
+		/* code */
+	}
+	
 }
